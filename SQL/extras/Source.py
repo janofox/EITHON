7702dbb6bdee8357ed31d4774c789252ecb8be30
janofox <105672710+janofox@users.noreply.github.com>
2022-05-21 15:33:08 +0300

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from userbot import StartTime, iqthon
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**𓆩𓆩⦙ كـيفـيه التـنصيـب:𓆪𓆪**")
+help2 = ("**𓆩𓆩⦙ قـائـمـه الاوامــر:𓆪𓆪**\n**𓆩𓆩⦙ مـطـوريـن الـسـورس:𓆪𓆪** @AlllIllIll 𖠄 @TTTL1 ** ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**.𓆩𓆩: الـاصـدار 1.1.1 𓆪𓆪.** \n.𓆩𓆩: انـت  {mention} 𓆪𓆪. \n**.𓆩𓆩: الـوقـت {TM} 𓆪𓆪.**\n**.𓆩𓆩: بـوتـي {TG_BOT} 𓆪𓆪.**\n**.𓆩𓆩: الـمـطـوريـن : @TTTL1 𖠄 @AlllIllIll 𓆪𓆪.**"
